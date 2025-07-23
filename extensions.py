369b7a041804150b5a891f8b08fadaf0ad73e88f
vitaly339 <matvienko.vitalik@list.ru>
2025-07-23 09:29:23 +0300

    Delete extensions.py

--- extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from sqlalchemy.orm import DeclarativeBase
-
-class Base(DeclarativeBase):
-    pass
-
-db = SQLAlchemy(model_class=Base)
-login_manager = LoginManager()
