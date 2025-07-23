d7504f5c00ceaaa4a0c3f38c765e97bc6a10cf7f
vitaly339 <matvienko.vitalik@list.ru>
2025-07-23 15:57:05 +0300

    Update project_models.py

--- project_models.py
+++ project_models.py
@@ -17,9 +17,9 @@ class MessageType(enum.Enum):
 
 class Admin(UserMixin, db.Model):  # Исправлено db.model на db.Model
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
     # Методы, необходимые для Flask-Login
